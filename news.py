c6564245e50b73db3a6207b0be4587e109572f2b
GARG <rahulgarg95@gmail.com>
2018-09-04 21:29:46 +0530

    Modification 5

--- news.py
+++ news.py
@@ -5,6 +5,8 @@ from flask import request
 import json
 import urllib
 import urllib2
+import datetime
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -22,30 +24,36 @@ DEFAULTS = {'publication': 'bbc',
             'currency_to': 'USD'
             }
 
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
 
 @app.route("/")
 def home():
     # get customised headlines, based on user input or default
-    publication = request.args.get('query')
-    if not publication:
-        publication = DEFAULTS['publication']
+    publication = get_value_with_fallback("query")
     articles = get_news(publication)
+
     # get customised weather based on user input or default
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
-    weather = get_weather(city)
+    city = get_value_with_fallback("city")
+    weather = get_weather (city)
+
     # get customised currency based on user input or default
-    currency_from = request.args.get("currency_from")
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get("currency_to")
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback("currency_from")
+    currency_to = get_value_with_fallback("currency_to")
     rate, currencies = get_rate(currency_from, currency_to)
-    return render_template("home.html", articles=articles, weather=weather,
-                           currency_from=currency_from, currency_to=currency_to, rate=rate,
-                           currencies=sorted(currencies))
+
+    # save cookies and return template
+    response = make_response(render_template("home.html", articles=articles, weather=weather, currency_from=currency_from, currency_to=currency_to, rate=rate, currencies=sorted(currencies)))
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_from", currency_from, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
 
 
 def get_rate(frm, to):
